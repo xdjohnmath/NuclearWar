4476a648276b8c9eddf1a115fce19277f68ae293
Adriel <richardd.oci12@gmail.com>
2016-10-31 16:02:55 -0200

    Criando Classes Manager

---
+++ Nuclear War/Assets/Scripts/Characters Manager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharactersManager : MonoBehaviour{
+
+    //All properties
+    public int creatureCost { get; set; }
+    public int playerLife { get; set; }
+    public int creatureLife { get; set; }
+    public int creatureAttack { get; set; }
+    public float creatureSpeed { get; set; }
+    public int playerEnergy { get; set; }
+    public int enemyEnergy { get; set; }
+    public string creatureName { get; set; }
+    public string creatureSpecialAttack { get; set; }
+    public enum creatureType {Tank, LowRange, HighRange};
+    public int creatureIniciative { get; set; }
+    public float energyTime { get; set; }
+
+    //value to be added and how long you must wait
+    public void EnergyPlayer (int value, float rate) {
+        energyTime -= Time.deltaTime;
+        if (energyTime < 0) {
+            playerEnergy += value;
+            energyTime = rate;
+        }
+    }
+
+    //value to be added and how long you must wait
+    public void EnergyEnemy (int value, float rate) {
+        energyTime -= Time.deltaTime;
+        if (energyTime < 0) {
+            enemyEnergy += value;
+            energyTime = rate;
+        }
+    }
+
+
+}
---
+++ Nuclear War/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : CharactersManager {
+    
+    //Singleton
+    public static PlayerManager instance = null;
+
+    void Awake () {
+       
+        //Singleton
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy (gameObject);
+        }
+        //EndOfSingleton
+
+        //Initialing variable. The "instance." is used to say we're referring to this object instead of the CharactersManager class.
+        instance.energyTime = 1;
+    }
+
+    void Update () {
+        EnergyPlayer (1, 2);
+    }
+}
