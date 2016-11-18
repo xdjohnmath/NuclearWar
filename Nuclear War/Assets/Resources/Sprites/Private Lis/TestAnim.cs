4fe1182d323ee79974691f29876a1e1816e2e33f
PC-JOÃO\João <xdjohnmath@gmail.com>
2016-11-18 00:23:22 -0300

    Private Lis

---
+++ Nuclear War/Assets/Resources/Sprites/Private Lis/TestAnim.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnim : MonoBehaviour {
+
+	private Animator a;
+
+	void Start () {
+		a = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.Space)){
+			a.SetInteger ("component", 1);
+		}
+
+		else if (Input.GetKey(KeyCode.B)){
+			a.SetInteger ("component", 3);
+		}
+
+	}
+
+	public void Walking () {
+		a.SetInteger ("component", 2);
+	}
+
+	public void Attacking () {
+		a.SetInteger ("component", 4);
+	}
+}
