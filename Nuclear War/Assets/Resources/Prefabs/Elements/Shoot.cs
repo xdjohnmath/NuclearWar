bdf24b753678c35eac5c9d762c5c3abced472009
PC-JOÃO\João <xdjohnmath@gmail.com>
2016-12-13 01:42:53 -0300

    É tiroo!

---
+++ Nuclear War/Assets/Resources/Prefabs/Elements/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject shootOBJ;
+
+	private Animator a;
+
+	void Start () {
+		a = GetComponent <Animator> ();
+	}
+	
+	void Update () {
+		if (shootOBJ.activeSelf == true) {
+			a.SetBool ("pow", true);
+		} else {
+			a.SetBool ("pow", false);
+		
+		}
+	}
+}
